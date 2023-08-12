88aaf446837ae071c5effdc1819eb6ccfbac7148
Andrew Gerez <andrewgerez18@gmail.com>
2023-08-11 23:37:53 -0300

    Feature: 8 | Create seasons controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\SeriesController;
+use App\Http\Controllers\SeasonsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +21,6 @@ Route::get('/', function () {
 
 Route::resource('/series', SeriesController::class)
     ->except(['show']);
+
+Route::get('/series/{series}/seasons', [SeasonsController:: class, 'index'])
+    ->name('seasons.index');
